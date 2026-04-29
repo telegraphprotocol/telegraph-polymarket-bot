f31932793930c69d32ba8dfca7dd27b29c9407e3
IamTalha-Sajid <iamtalha.sajid@gmail.com>
2026-04-29 13:31:54 +0500

    [FIX]: Fixed build issues

--- landing-page/next.config.ts
+++ landing-page/next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
-// @ts-ignore
-if (typeof global !== 'undefined' && global.localStorage && !global.localStorage.getItem) {
-  // @ts-ignore
-  delete global.localStorage;
-}
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
